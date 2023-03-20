f0ec1fb1f8fbd91517a6e667e2c079aa9f76d30b
rijalislamir <rijalislamir@gmail.com>
2023-03-20 21:31:07 +0700

    uninstall prisma

--- main.js
+++ main.js
@@ -1,69 +1,33 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-
 const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.post('/users', async (req, res) => {
-  const { name, email, phone } = req.body
-
-  if (!name || !email || !phone ) {
-    const fields = []
-    res.statusCode = 400
-    
-    if (!name) fields.push('name')
-    if (!email) fields.push('email')
-    if (!phone) fields.push('phone')
-
-    return res.send({
-      error: `Cannot found [${fields.map((field, i) => i ? ' ' + field : field)}] field${fields.length > 1 ? 's' : ''}!`
-    })
-  }
-
-  const newUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      phone
-    }
+app.get('/users', async (req, res) => {
+  return res.send({
+    method: 'GET',
+    endpoint: '/users'
   })
-
-  return res.send(newUser)
 })
 
 app.delete('/users', async (req, res) => {
-  const { id } = req.body
-
-  if (typeof id !== 'string') {
-    res.statusCode = 400
-    
-    return res.send({
-      error: `Wrong type of id. Should be string!`
-    })
-  }
-
-  if (!id) {
-    res.statusCode = 400
-    
-    return res.send({
-      error: `Cannot found id!`
-    })
-  }
-
-  const deletedUser = await prisma.user.deleteMany({
-    where: { id }
+  return res.send({
+    method: 'DELETE',
+    endpoint: '/users'
   })
+})
 
-  if (!deletedUser.count) {
-    res.statusCode = 400
+app.put('/users', async (req, res) => {
+  return res.send({
+    method: 'PUT',
+    endpoint: '/users'
+  })
+})
 
-    return res.send({
-      error: `Cannot found user!`
-    })
-  }
-  
-  return res.send({ id })
+app.delete('/users', async (req, res) => {
+  return res.send({
+    method: 'DELETE',
+    endpoint: '/users'
+  })
 })
 
 app.listen(5000)
