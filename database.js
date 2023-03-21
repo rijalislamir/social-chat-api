ac41314c3705703600d0bde2d430a0efb7c8c61f
rijalislamir <rijalislamir@gmail.com>
2023-03-21 21:26:17 +0700

    use pool instead of connection

--- database.js
+++ database.js
@@ -3,17 +3,11 @@ require('dotenv').config()
 const mysql = require('mysql2')
 
 const connectDb = () => {
-  const connection = mysql.createConnection({
+  const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-  })
-  
-  connection.connect((err) => {
-    if (err) throw err
-
-    console.log('MySQL database is connected!')
   })
 
   return connection
