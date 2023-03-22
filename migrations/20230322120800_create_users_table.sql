89fec5b4d921a197ea7dc555f4d5a86b29430d04
rijalislamir <rijalislamir@gmail.com>
2023-03-22 13:13:29 +0700

    add db schema migrations folder

---
+++ migrations/20230322120800_create_users_table.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ migrations/20230322120900_add_password_field.sql
@@ -0,0 +1,1 @@
+ALTER TABLE social_chat.users ADD password varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
