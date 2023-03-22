9ca34023a7aa3c7a9b2e935a7135492d1bfdbf56
rijalislamir <rijalislamir@gmail.com>
2023-03-22 14:41:48 +0700

    modify add password field sql

--- migrations/20230322120900_add_password_field.sql
+++ migrations/20230322120900_add_password_field.sql
@@ -1,4 +1,4 @@
 
 -- add_password_field
 
-ALTER TABLE social_chat.users ADD password varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE users ADD password varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
