6d5c17903075f55ce8d4b17505fd619984b10394
rijalislamir <rijalislamir@gmail.com>
2023-03-22 15:50:32 +0700

    change primary key from id to email

--- migrations/20230322120800_create_users_table.sql
+++ migrations/20230322120800_create_users_table.sql
@@ -7,5 +7,6 @@ CREATE TABLE `users` (
   `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- migrations/20230322120900_add_password_field.sql
+++
@@ -1,4 +0,0 @@
-
--- add_password_field
-
-ALTER TABLE users ADD password varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
