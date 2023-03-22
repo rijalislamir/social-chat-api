315304ad7622323837695e91e7021613a75aeb72
rijalislamir <rijalislamir@gmail.com>
2023-03-22 22:26:29 +0700

    install cors package

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/', authRoutes)
